bfd3298ce106bf2caf1de6659935886f4b882d82
Matheus <matheus-holanda@hotmail.com>
2019-06-02 16:23:31 -0300

    Update: Dijkstra

--- Grafo/grafo.h
+++ Grafo/grafo.h
@@ -1,8 +1,14 @@
 #define true 1
 #define false 0
-typedef int boll;
+
+#define BRANCO 0
+#define AMARELO 1
+#define VERMELHO 2
+
+typedef int bool;
 typedef int TIPOPESO;
 
+/* FUNÇÕES PARA CRIAR O GRAFO*/
 typedef struct adjacencia{
     int vertice;  //Vetice Final
     TIPOPESO peso; //Peso ou Caminho
@@ -39,7 +45,7 @@ ADJACENCIA *criaAdj(int v, int peso){
     return(temp);
 }
 
-boll criaAresta(GRAFO *gr,int vi,int vf, TIPOPESO p){
+bool criaAresta(GRAFO *gr,int vi,int vf, TIPOPESO p){
     if(!gr) return(false);
     if((vf<0)||(vf>= gr->vertices))
         return(false);
@@ -66,17 +72,85 @@ void imprime(GRAFO *gr){
     }
 }
 
-/*void contarGrauEntrada(int v){}
-void contarGrauSaida(int v){}
-void contarGrauTotal(int v){}
+/*FUNÇÕES PARA BUSCA EM PROFUNDIDADE (DFS)*/
 
-void InserirVertice(){}
-void RemoveVertice(){}
+/*FUNÇÕES PARA BUSCA EM LARGURA (BFS)*/
 
-void InserirAresta(){}
-void RemoverAresta(){}
+/*ALGORITMO DE DIJKSTRA*/
 
-void ImprimeGrafo(){}*/
+//O algoritmo de Dijkstra soluciona o problema
+//do caminho mais curto num grafo dirigido
+//ou nao dirigido, com arestas de peso não negativo
+void inicializaD(GRAFO *g, int *d, int *p, int s){
+    int v;
+    for(v=0; v<g->vertices;v++){
+        d[v] = INT_MAX/2;
+        p[v] = -1;
+    }
+    d[s] = 0;
+}
+
+void relaxa(GRAFO *g, int *d, int *p, int u, int v){
+    ADJACENCIA *ad = g->adj[u].cab;
+    while(ad && ad->vertice != v)
+        ad = ad->prox;
+    if(ad){
+        if(d[v]>d[u] + ad->peso){
+            d[v] = d[u] + ad->peso;
+            p[v] = u;
+        }
+    }
+}
+
+int *dijkstra(GRAFO *g, int s){
+    int *d = (int *) malloc(g->vertices* sizeof(int));
+    int p[g->vertices];
+    bool aberto[g->vertices];
+    inicializaD(g,d,p,s);
+
+    int i;
+    for(i=0;i<g->vertices;i++)
+        aberto[i] = true;
+
+    while(existeAberto(g,aberto)){
+        int u = menorDist(g,aberto,d);
+        aberto[u] = false;
+        ADJACENCIA *ad = g->adj[u].cab;
+        while(ad){
+            relaxa(g,d,p,u,ad->vertice);
+            ad = ad->prox;
+        }
+    }
+    return(d);
+}
+
+//Varre o arranjo de abertos
+//Se encontrar um true retorna true
+//Ou seja que achou, se não retorna false
+bool existeAberto(GRAFO *g, int *aberto){
+    int i;
+    for(i=0;i<g->vertices;i++)
+        if(aberto[i]) return(true);
+    return(false);
+}
+
+//Dentre todos os abertos necessita
+//achar oque possui menor distância
+int menorDist(GRAFO *g, int *aberto, int *d){
+    int i;
+    for(i=0; i<g->vertices; i++)
+        if(aberto[i]) break;
+    if(i==g->vertices) return(-1);
+    int menor = i;
+    for(i=menor+1; i<g->vertices;  i++)
+        if(aberto[i] && (d[menor]>d[i]))
+            menor = i;
+        return(menor);
+}
+
+/*FUNÇÕES PARA EXCLUIR*/
+
+/*https://www.includehelp.com/ds/addition-and-deletion-of-nodes-and-edges-in-a-graph-using-adjacency-matrix.aspx*/
 
 char menu_principal()
 {
@@ -90,3 +164,15 @@ char menu_principal()
     printf("  ESCOLHA UM OP%c%cO: ",128,199);
     return getche();
 }
+
+/*void contarGrauEntrada(int v){}
+void contarGrauSaida(int v){}
+void contarGrauTotal(int v){}
+
+void InserirVertice(){}
+void RemoveVertice(){}
+
+void InserirAresta(){}
+void RemoverAresta(){}
+
+void ImprimeGrafo(){}*/
--- Grafo/main.c
+++ Grafo/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "grafo.h"
 
 int main(){
     char op, op2, op3;
-    int num, vi, vf, p;
+    int num, vi, vf, p, i, d;
     GRAFO *gr;
+    int *r;
 
     while(1){
         printf("\n********************************\n");
@@ -30,6 +32,7 @@ int main(){
             	fflush(stdin);
 
             	printf("\n  Grafo criado com sucesso!\n");
+
             	break;
 
             case '2':
@@ -48,12 +51,16 @@ int main(){
 
                 criaAresta(gr, vi, vf, p);
 
-                /*criaAresta(gr,0,1,2);
-                criaAresta(gr,1,2,4);
-                criaAresta(gr,2,0,12);
-                criaAresta(gr,2,4,40);
-                criaAresta(gr,3,1,3);
-                criaAresta(gr,4,3,8);*/
+                /*criaAresta(gr, 0, 1, 10);
+                criaAresta(gr, 0, 2, 5);
+                criaAresta(gr, 2, 1, 3);
+                criaAresta(gr, 1, 3, 1);
+                criaAresta(gr, 2, 3, 8);
+                criaAresta(gr, 2, 4, 2);
+                criaAresta(gr, 4, 5, 6);
+                criaAresta(gr, 3, 5, 4);
+                criaAresta(gr, 3, 4, 4);*/
+
                 break;
 
             case '3':
@@ -61,9 +68,22 @@ int main(){
 				break;
 
             case '4':
+
 				break;
 
             case '5':
+
+                /*DIJKSTRA*/
+                printf("\n  Digite o n%c inicial: ", 224);
+                scanf("%i",&d);
+                fflush(stdin);
+
+                r = dijkstra(gr, d);
+
+                for(i = 0; i < gr->vertices; i++){
+                    printf("  D(v%d -> v%d) = %d\n", d, i, r[i]);
+                }
+
                 break;
 
             case '6':
@@ -84,6 +104,7 @@ int main(){
         }
     }
 
+    free(r);
     free(gr);
 
     return 0;
